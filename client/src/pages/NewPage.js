22d1944fd13149221f2ccc1d66fcab29a2cbb27b
Andrey Filatkin <anfilat@mail.ru>
2020-09-24 14:24:13 +0300

    Fix editor styles

--- client/src/pages/NewPage.js
+++ client/src/pages/NewPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import {Box, Container} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 import {Title} from '../components/Title';
 import {useBindLocalStorage} from '../hooks/bindLocalStorage.hook';
 const Editor = require('../ckeditor/build/ckeditor');
+
+const useStyles = makeStyles(theme => ({
+    editor: {
+        '& .ck-editor': {
+            height: `calc(100vh - 64px - ${theme.spacing(4)}px)`,
+            display: 'flex',
+            'flex-direction': 'column',
+            '& .ck-editor__main': {
+                'flex-grow': 1,
+                overflow: 'auto',
+                '& .ck-content': {
+                    height: '100%',
+                }
+            }
+        }
+    },
+}));
 
 const config = {
     toolbar: [
@@ -11,11 +29,13 @@ const config = {
         "bold", "italic", "strikethrough",  "|",
         "link", "bulletedList", "numberedList", "|",
         "alignment", "indent", "outdent", "|",
-        "blockQuote", "codeBlock", "code", "insertTable", "undo", "redo"
+        "code", "codeBlock", "insertTable", "|",
+        "undo", "redo"
     ],
 };
 
 export const NewPage = () => {
+    const classes = useStyles();
     const [content, setContent] = useBindLocalStorage('newData', 'content', '');
 
     function changeHandler(event, editor) {
@@ -26,7 +46,7 @@ export const NewPage = () => {
         <>
             <Title title="New"/>
             <Container component="main" maxWidth="md">
-                <Box mt={2}>
+                <Box mt={2} className={classes.editor}>
                     <CKEditor
                         editor={ Editor }
                         config={config}
