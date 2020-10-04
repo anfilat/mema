6ffee37bafae761c18aa4825e4e04bdf8844de49
Andrey Filatkin <anfilat@mail.ru>
2020-10-04 15:51:33 +0300

    More tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,22 +1,30 @@
 const request = require('supertest');
 const app = require('../app');
 
-test('index.html', async () => {
+test('get index.html', async () => {
     await request(app)
         .get('/')
         .expect('Content-Type', /text\/html/)
-        .expect(200);
+        .expect(200)
+        .expect(({text}) => expect(text).toMatchSnapshot());
 });
 
-test('some page', async () => {
+test('get some page html', async () => {
     await request(app)
         .get('/name')
         .expect('Content-Type', /text\/html/)
-        .expect(200);
+        .expect(200)
+        .expect(({text}) => expect(text).toMatchSnapshot());
 });
 
-test('/api/wrong', async () => {
+test('api wrong endpoint', async () => {
     await request(app)
         .get('/api/wrong')
+        .expect(404);
+});
+
+test('api wrong verb', async () => {
+    await request(app)
+        .get('/api/auth/login')
         .expect(404);
 });
