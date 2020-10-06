714739d344a2ed4e7e2fd719f4df63d6a19d04b6
Andrey Filatkin <anfilat@mail.ru>
2020-10-06 13:04:39 +0300

    Change timestamp fields to timestamptz

--- server/db.sql
+++ server/db.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS text (
     text_id serial PRIMARY KEY,
     account_id int NOT NULL,
     text TEXT,
-    time timestamp NOT NULL,
+    time timestamptz NOT NULL,
 
     CONSTRAINT text_account_id_fk FOREIGN KEY (account_id) REFERENCES account(account_id)
 );
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS mem (
     mem_id serial PRIMARY KEY,
     text_id int NOT NULL,
     account_id int NOT NULL,
-    create_time timestamp NOT NULL,
-    last_change_time timestamp NOT NULL,
+    create_time timestamptz NOT NULL,
+    last_change_time timestamptz NOT NULL,
 
     CONSTRAINT mem_text_id_fk FOREIGN KEY (text_id) REFERENCES text(text_id),
     CONSTRAINT mem_account_id_fk FOREIGN KEY (account_id) REFERENCES account(account_id)
