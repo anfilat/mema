d856f0498f4e14d683d843e7ff072c4036eb4f15
Andrey Filatkin <anfilat@mail.ru>
2021-01-14 18:08:47 +0300

    Fix: wrong month in a item header

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -44,7 +44,7 @@ export default function Item(props) {
 function renderTime(time) {
     const date = new Date(time);
     const year = String(date.getFullYear()).padStart(4, '0');
-    const month = String(date.getMonth()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
